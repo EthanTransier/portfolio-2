91be33b9e3d757de4b0fd95ed535f1fd9df8aa2a
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2023-05-24 14:13:38 -0700

    Finished Portfolio-2

--- index.js
+++ index.js
@@ -31,6 +31,9 @@ function fishInteract(){
   let rect1 = document.getElementById('hook').getBoundingClientRect();
   let NFrect2 = document.getElementById('normFish').getBoundingClientRect();
   let Srect2 = document.getElementById('shark').getBoundingClientRect();
+  let Jrect2 = document.getElementById('jFish').getBoundingClientRect();
+  let Swrect2 = document.getElementById('sFish').getBoundingClientRect();
+
     var normFishOverlap = !(
       rect1.right < NFrect2.left || 
       rect1.left > NFrect2.right || 
@@ -44,8 +47,23 @@ function fishInteract(){
       rect1.bottom < Srect2.top || 
       rect1.top > Srect2.bottom
     )
+
+    var jFishOverlap = !(
+      rect1.right < Jrect2.left || 
+      rect1.left > Jrect2.right || 
+      rect1.bottom < Jrect2.top || 
+      rect1.top > Jrect2.bottom
+    )
+
+    var sFishOverlap = !(
+      rect1.right < Swrect2.left || 
+      rect1.left > Swrect2.right || 
+      rect1.bottom < Swrect2.top || 
+      rect1.top > Swrect2.bottom
+    )
   
   if(normFishOverlap == true){
+    pageLocation = 2
     $('#projectsTabID').css({'height': '100vh', 'display': 'flex'})
     gameStarted = false;
     scrolledUp = true;
@@ -75,6 +93,7 @@ function fishInteract(){
     
     document.getElementById('projectsTabID').scrollIntoView();
   }else if(sharkOverlap == true && normFishOverlap == false){
+    pageLocation = 2
     $('#resumeTabID').css({'height': '100vh', 'display': 'flex'})
     gameStarted = false;
     scrolledUp = true;
@@ -104,6 +123,68 @@ function fishInteract(){
     $(window).off();
     
     document.getElementById('resumeTabID').scrollIntoView();
+  }else if(jFishOverlap == true && sFishOverlap == false){
+    pageLocation = 2
+    $('#aboutTabID').css({'height': '100vh', 'display': 'flex'})
+    gameStarted = false;
+    scrolledUp = true;
+  
+    $('.hook').animate({'top': 0,'left': window.innerWidth * .5}, 500)
+    $('.hookLine').animate({'height': 0,'left': window.innerWidth * .5}, 500)
+
+    setTimeout(function(){
+      $('.hook').css('left', '45.5vw')
+      $('.hook').css('top', '0')
+      $('.hook').addClass('hookAnimation');
+      $('.hookLine').css('height', 0)
+      $('.hookLine').css('left', 0)
+      $('.hookLine').css('top', '-30vh')
+    }, 500)
+
+    let jFishTop = document.getElementById('jFish').getBoundingClientRect().top;
+    let jFishLeft = document.getElementById('jFish').getBoundingClientRect().left;
+
+    $('.jFish').css('top', jFishTop + vh(250))
+    $('.jFish').css('left', jFishLeft)
+    
+    $('.jFish').removeClass('jFishAnimation');
+
+    $('.jFish').animate({'top': 0, 'left': window.innerWidth * .5}, 500)
+    console.log(vh(-30))
+    $(window).off();
+    
+    document.getElementById('aboutTabID').scrollIntoView();
+  }else if(sFishOverlap == true){
+    pageLocation = 2
+    $('#contactTabID').css({'height': '100vh', 'display': 'flex'})
+    gameStarted = false;
+    scrolledUp = true;
+  
+    $('.hook').animate({'top': 0,'left': window.innerWidth * .5}, 500)
+    $('.hookLine').animate({'height': 0,'left': window.innerWidth * .5}, 500)
+
+    setTimeout(function(){
+      $('.hook').css('left', '45.5vw')
+      $('.hook').css('top', '0')
+      $('.hook').addClass('hookAnimation');
+      $('.hookLine').css('height', 0)
+      $('.hookLine').css('left', 0)
+      $('.hookLine').css('top', '-30vh')
+    }, 500)
+
+    let sFishTop = document.getElementById('sFish').getBoundingClientRect().top;
+    let sFishLeft = document.getElementById('sFish').getBoundingClientRect().left;
+
+    $('.sFish').css('top', sFishTop + vh(250))
+    $('.sFish').css('left', sFishLeft)
+    
+    $('.sFish').removeClass('sFishAnimation');
+
+    $('.sFish').animate({'top': 0, 'left': window.innerWidth * .5}, 500)
+    console.log(vh(-30))
+    $(window).off();
+    
+    document.getElementById('contactTabID').scrollIntoView();
   }
 })
 }
@@ -152,6 +233,8 @@ window.addEventListener('keydown', function(e) {
 
       $('.normFish').addClass('normFishAnimation');
       $('.shark').addClass('sharkAnimation');
+      $('.jFish').addClass('jFishAnimation');
+      $('.sFish').addClass('sFishAnimation');
 
       $('.hookLine').css('transform', 'translate(-1vw, 1vh)')
       
@@ -181,27 +264,33 @@ window.addEventListener('keydown', function(e) {
 
       
       $('.hook').animate({'top': 2560,'left': window.innerWidth * .4445}, 750)
-      // console.log(distance)
-      // console.log(hookLineTop)
-
-      // let distance2 = 2560 - hookLineTop
-      
-      // // $('.hookLine').css('height', distance)
 
       $('.hookLine').animate({'left': window.innerWidth * .5, 'height': 2560 - array1[1]}, 750)
       console.log(hookLineTop)
-      // // $('.hookLine').css('transform', 'translateY(-135vh)')
       let animateHook = setTimeout(finishHookAnimation, 750)
       e.preventDefault();
     }
   }else if(e.keyCode == '40'){
     if(pageLocation == 3){
       document.getElementById('secondFishArea').scrollIntoView()
+      $('.hook').animate({'top': vh(550),'left': window.innerWidth * .4445}, 750)
+
+      $('.hookLine').animate({'left': window.innerWidth * .5, 'height': vh(550) - array1[1]}, 750)
+      pageLocation = 4
+    }
+  }else if(e.keyCode == '38'){
+    console.log('up arrow pressed')
+    if(pageLocation == 4){
+      $('.hook').animate({'top': 2560,'left': window.innerWidth * .4445}, 750)
+
+      $('.hookLine').animate({'left': window.innerWidth * .5, 'height': 2560 - array1[1]}, 750)
+      startGame();
     }
   }
 });
 
 function startGame(){
+  pageLocation = 3;
   document.getElementById('beach').scrollIntoView();
 }
 var mouseX = window.innerWidth / 2;
@@ -230,7 +319,10 @@ window.addEventListener('scroll', function() {
 
 function finishHookAnimation(){
   $('#resumeTabID').css({'height': '0', 'display': 'none'})
-      $('#projectsTabID').css({'height': '0', 'display': 'none'})
+  $('#projectsTabID').css({'height': '0', 'display': 'none'})
+  $('#aboutTabID').css({'height': '0', 'display': 'none'})
+  $('#contactTabID').css({'height': '0', 'display': 'none'})
+
   $(window).on("click", function(){
      console.log(mouseX + " " + mouseY)
 
